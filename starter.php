adeb0da5795f1dcc605c0415ae2a5ad5848e1634
Bryan360 <bryan@360recruitment.nl>
2017-06-01 15:51:24 +0200

    starter aangemaakt

---
+++ starter.php
@@ -0,0 +1,147 @@
+<?php require 'header.php'; ?>
+<link href="css/index.min.css" type="text/css" rel='stylesheet'>
+<style></style>
+<div class="fluid-container">
+   <div class='single-slide' id="first-block">
+      <?php require 'navbar.php'; ?>
+      <div class='visible-xs'>
+          <h1 class='text-center'>Profesionele website vanaf 499,-</p>
+      </div>
+   </div>
+</div>
+
+<div class='single-slide' id="second-block">
+   <div class='col-md-12 col-xs-12 block-back'>
+      <div id="container">
+         <div class="whole">
+            <div class="type">
+               <p>Starter</p>
+            </div>
+            <div class="plan">
+               <div class="header">
+                  <span>€</span>499
+                  <p class="month">€29 per maand</p>
+               </div>
+               <div class="content">
+                  <ul>
+                     <li>WordPress CMS</li>
+                     <li>Keuze uit Templates</li>
+                     <li>5 GB Dataverkeer</li>
+                     <li>8 GB Schrijfruimte</li>
+                     <li>1 MYSQL Database</li>
+                     <li>Shared Server</li>
+                     <li>Domeinregistratie</li>
+                     <li>10 Emailboxen</li>
+                     <li>2 GB Email geheugen</li>
+                     <li>Full Service Support</li>
+                     <li> - </li>
+                  </ul>
+               </div>
+               <a href="https://www.360recruitment.nl/360web/starter" class="more-link">
+                  <div class='more-button'>
+                     Bekijk
+                  </div>
+               </a>
+            </div>
+         </div>
+         <div class="whole">
+            <div class="type standard">
+               <p>Basic</p>
+            </div>
+            <div class="plan">
+               <div class="header">
+                  <span>€</span>999
+                  <p class="month">€39 per maand</p>
+               </div>
+               <div class="content">
+                  <ul>
+                     <li>WordPress CMS</li>
+                     <li>Template met maatwerk</li>
+                     <li>15 GB Dataverkeer</li>
+                     <li>25 GB Schrijfruimte</li>
+                     <li>2 MYSQL Databases</li>
+                     <li>VPN server</li>
+                     <li>Domeinregistratie</li>
+                     <li>40 Emailboxen</li>
+                     <li>10 GB Email geheugen</li>
+                     <li>Full Service Support</li>
+                     <li>+ 4 extra's</li>
+                  </ul>
+               </div>
+               <a href="https://www.360recruitment.nl/360web/basic" class="more-link">
+                  <div class='more-button'>
+                     Bekijk
+                  </div>
+               </a>
+            </div>
+         </div>
+         <div class="whole ">
+            <div class="type ultimate">
+               <p>Premium</p>
+            </div>
+            <div class="plan">
+               <div class="header">
+                  <span>€</span>2.499
+                  <p class="month">€69 per maand</p>
+               </div>
+               <div class="content">
+                  <ul>
+                     <li>Bootstrap Platform</li>
+                     <li>Eigen design</li>
+                     <li>Onbeperkte Dataverkeer</li>
+                     <li>95 GB Schrijfruimte</li>
+                     <li>10 MYSQL Databases</li>
+                     <li>VPN server</li>
+                     <li>Domeinregistratie</li>
+                     <li>80 Emailboxen</li>
+                     <li>35 GB Email geheugen</li>
+                     <li>Dedicated Full Service Support</li>
+                     <li>+ 15 extra's</li>
+                  </ul>
+               </div>
+               <a href="https://www.360recruitment.nl/360web/premium" class="more-link">
+                  <div class='more-button'>
+                     Bekijk
+                  </div>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class='popup hidden-xs'><i class="fa fa-times" aria-hidden="true" id="hide"></i> Wilt u zelf een pakket samen stellen? <br><a href="https://www.360recruitment.nl/360web/calculator">Klik hier!</a></div>
+</div>
+<div class='double-slide' id='third-block'>
+   <div class='main'><h2 class='flex-center'>IN DRIE STAPPEN EEN GEWELDIGE WEBSITE</h2></div>
+   <div class='main'>
+       <div class='col-md-10 col-md-offset-1'>
+           <div class='col-md-4'>
+               <h2 class='text-center'>KIES HET PRODUCT</h2>
+               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec.</p>
+           </div>
+           <div class='col-md-4'>
+               <h2 class='text-center'>U STUURT DE CONTENT</h2>
+               <p>
+                   Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat.
+
+</p>
+           </div>
+           <div class='col-md-4'>
+               <h2 class='text-center'>LIVE!</h2>
+
+               <p>
+                   Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing.
+               </p>
+           </div>
+       </div>
+   </div>
+</div>
+<div class='single-slide' id='fourth-block'>
+   <div class="smallspace"></div>
+   <div class='col-md-10 col-md-offset-1 col-xs-12' >
+      <?php include('fullservice.php'); ?>
+   </div>
+</div>
+<div class='back-to-top' id='back-to-top' title='Back to top'><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+<?php require 'footer.php'; ?>
+</div>
+</body>
