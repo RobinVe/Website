670050b43b04ec3320bc28ec218e81702db47ec0
Bryan360 <bryan@360recruitment.nl>
2017-06-02 11:04:13 +0200

    pakket starter

---
+++ starter.php
@@ -0,0 +1,128 @@
+<?php require 'header.php'; ?>
+<link href="css/index.min.css" type="text/css" rel='stylesheet'>
+<link href="css/pakketten.css" type="text/css" rel='stylesheet'>
+<style>
+
+
+</style>
+<div class="fluid-container">
+      <?php require 'navbar.php'; ?>
+</div>
+
+<div class='double-slide'>
+   <div class='main'>
+	   <h2 class='flex-center'>STARTER</h2>
+   </div>
+   <div class='main'>
+       <div class='col-md-10 col-md-offset-1'>
+         <div class='row'>
+    <div class='col-md-12 col-lg-3'>
+      <div class='col-md-10 col-md-offset-1 header'>
+        Vormgeving
+      </div>
+      <div class='content col-md-10 col-md-offset-1'>
+        <ul>
+          <li><i class="fa fa-check" aria-hidden="true"></i> WordPress CMS </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> Keuze uit Templates </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <a href="https://www.360recruitment.nl/360web/basic">
+      <div class="col-md-10 col-md-offset-1 button">
+      bekijk basic
+    </div>
+  </a>
+    </div>
+    <div class='col-md-12 col-lg-3'>
+      <div class='col-md-10 col-md-offset-1 header'>
+        Data
+      </div>
+      <div class='content col-md-10 col-md-offset-1'>
+        <ul>
+          <li><i class="fa fa-check" aria-hidden="true"></i> 5 GB Dataverkeer </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> 8 GB Schrijfruimte </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> 1 MYSQL Database </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> Shared Server </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> Domeinregistratie </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+        </ul>
+      </div>
+      <div class='col-md-10 col-md-offset-1 button3'>
+      </div>
+    </div>
+    <div class='col-md-12 col-lg-3'>
+      <div class='col-md-10 col-md-offset-1 header'>
+        E-mail
+      </div>
+      <div class='content col-md-10 col-md-offset-1'>
+        <ul>
+          <li><i class="fa fa-check" aria-hidden="true"></i> 10 Emailboxen </li>
+          <li><i class="fa fa-check" aria-hidden="true"></i> 2 GB Email geheugen </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+        </ul>
+      </div>
+      <div class='col-md-10 col-md-offset-1 button3'>
+      </div>
+    </div>
+    <div class='col-md-12 col-lg-3'>
+      <div class='col-md-10 col-md-offset-1 header'>
+        Extra's
+      </div>
+      <div class='content col-md-10 col-md-offset-1'>
+        <ul>
+          <li><i class="fa fa-check" aria-hidden="true"></i> Full Service Support </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <a href="https://www.360recruitment.nl/360web/premium">
+      <div class="col-md-10 col-md-offset-1 button">
+      bekijk premium
+    </div>
+  </a>
+    </div>
+    </div>
+    <div class="row">
+      <div class='col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3'>
+        <a href="https://www.360recruitment.nl/360web/aanvraag">
+          <div class="col-md-12 button2">
+            vraag offerte aan
+          </div>
+        </a>
+      </div>
+    </div>
+       </div>
+   </div>
+</div>
+<div class='back-to-top' id='back-to-top' title='Back to top'><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+<?php require 'footer.php'; ?>
+</div>
+</body>
