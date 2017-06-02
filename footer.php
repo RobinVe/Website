a2f09da2b236c6e353e0041726a99a4970b68415
RobinVe <robin@360recruitment.nl>
2017-06-02 16:00:00 +0200

    fi88nal changes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class='back-to-top' id='back-to-top' title='Back to top'><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+<script src="<?php echo current_link();?>js/totop.js"></script>
+<script src="<?php echo current_link();?>js/scroll.js"></script>
 <footer>
-	<p>360Web | 2017<p>
+	<p>© 360Web | <?php echo date('Y');?> | Diamantlaan 41, Hoofddorp 2132 WV<p>
 </footer>
-<script src="js/totop.js"></script>
-<script src="js/scroll.js"></script>
--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,24 @@
+$(function () {
+    var top = $('.navbar').offset().top - parseFloat($('.navbar').css('marginTop').replace(/auto/, 0));
+    var footTop = $('#footer').offset().top - parseFloat($('#footer').css('marginTop').replace(/auto/, 0));
+    var maxY = footTop - $('.navbar').outerHeight();
+
+    $(window).scroll(function (evt) {
+        var y = $(this).scrollTop();
+        if (y > top) {
+            if (y < maxY) {
+                $('.navbar').addClass('fixed').removeAttr('style');
+            } else {
+                $('.navbar').removeClass('fixed').css({
+                    position: 'absolute',
+                    top: (maxY - top) + 'px'
+                });
+            }
+        } else {
+            $('.navbar').removeClass('fixed');
+        }
+    });
+});
 
 (function () {
     $(document).ready(function () {
