50dccb7d4b2e04808fe778ab3ac3c7acbaa3a20e
Bryan360 <bryan@360recruitment.nl>
2017-06-02 11:19:00 +0200

    PAKKETTEN
    Misschien moet iemand nog effe kijken want die blokken zijn echt fucking
    lang!

---
+++ premium.php
@@ -0,0 +1,129 @@
+<?php require 'header.php'; ?>
+<link href="css/index.min.css" type="text/css" rel='stylesheet'>
+<link href="css/pakketten.css" type="text/css" rel='stylesheet'>
+<style>
+
+
+</style>
+<div class="fluid-container">
+      <?php require 'navbar.php'; ?>
+</div>
+
+<div class='double-slide'>
+   <div class='main'>
+	   <h2 class='flex-center'>PREMIUM</h2>
+   </div>
+   <div class='main'>
+       <div class='col-md-10 col-md-offset-1'>
+         <div class='row'>
+  <div class='col-md-12 col-lg-3'>
+    <div class='col-md-10 col-md-offset-1 header'>
+      Vormgeving
+    </div>
+    <div class='content col-md-10 col-md-offset-1'>
+      <ul>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Bootstrap Platform </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Eigen design </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Formulieren </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Zoekfunctie </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Blog & Nieuwsberichten </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Testimonials </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Google Maps </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Social Media deelbuttons </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <a href="https://www.360recruitment.nl/360web/starter">
+    <div class="col-md-10 col-md-offset-1 button">
+    bekijk starter
+  </div>
+</a>
+  </div>
+  <div class='col-md-12 col-lg-3'>
+    <div class='col-md-10 col-md-offset-1 header'>
+      Data
+    </div>
+    <div class='content col-md-10 col-md-offset-1'>
+      <ul>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Onbeperkte Dataverkeer </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> 95 GB Schrijfruimte </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> 10 MYSQL Database </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> VPN Server </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Domeinregistratie </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+      </ul>
+    </div>
+    <div class='col-md-10 col-md-offset-1 button3'>
+    </div>
+  </div>
+  <div class='col-md-12 col-lg-3'>
+    <div class='col-md-10 col-md-offset-1 header'>
+      E-mail
+    </div>
+    <div class='content col-md-10 col-md-offset-1'>
+      <ul>
+        <li><i class="fa fa-check" aria-hidden="true"></i> 80 Emailboxen </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> 35 GB Email geheugen </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+      </ul>
+    </div>
+    <div class='col-md-10 col-md-offset-1 button3'>
+    </div>
+  </div>
+  <div class='col-md-12 col-lg-3'>
+    <div class='col-md-10 col-md-offset-1 header'>
+      Extra's
+    </div>
+    <div class='content col-md-10 col-md-offset-1'>
+      <ul>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Dedicated Full Service Support </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Technische Zoekmachine Optimalisatie </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Inhoudelijke Zoekmachine Optimalisatie </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> URL behoud </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Download functionaliteit </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Cookie controle </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> RSS Feed </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Google Analytics </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Mailchimp koppeling </li>
+        <li><i class="fa fa-check" aria-hidden="true"></i> Online KPI management </li>
+      </ul>
+    </div>
+    <a href="https://www.360recruitment.nl/360web/basic">
+    <div class="col-md-10 col-md-offset-1 button">
+    bekijk basic
+  </div>
+</a>
+  </div>
+  </div>
+  <div class="row">
+    <div class='col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3'>
+      <a href="https://www.360recruitment.nl/360web/aanvraag">
+        <div class="col-md-12 button2">
+          vraag offerte aan
+        </div>
+      </a>
+    </div>
+  </div>
+
+       </div>
+   </div>
+</div>
+<div class='back-to-top' id='back-to-top' title='Back to top'><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+<?php require 'footer.php'; ?>
+</div>
+</body>
