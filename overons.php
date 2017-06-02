bef8e8fc561ea3fca5fbb7eea7b14b8e77df3e93
RicardoHemprig <ricardo@360recruitment.nl>
2017-06-02 10:47:19 +0200

    Over ons
    Plaatjes + tekst

--- overons.php
+++ overons.php
@@ -7,55 +7,68 @@
     <?php require 'navbar.php'; ?>
     <div class="largespace"></div>
     <div class="row">
-      <div class="vertical-slide">
+      <div class="double-slide">
+        <div class="main">
       <div class="col-md-4">
+        <div class="mediumspace"></div>
         <div class='col-md-9 col-md-offset-1 text-center contact'>
-        <img src="img/pf/Robin_nieuw-225x300.png">
+        <img src="img/Robin_nieuw-225x300.png" height="250">
         <p>Robin Veer</p>
         <p class="quote">"You have to be perfect, to be amazing."</p>
 </div>
       </div>
       <div class="col-md-4">
+        <div class="mediumspace"></div>
+
         <div class='col-md-9 col-md-offset-2 text-center contact'>
-        <img src="img/pf/Ricardo_255x300.jpg">
+        <img src="img/Ricardo_255x300.jpg" height="250">
         <p>Ricardo Hemprig</p>
         <p class="quote">"Maybe swearing will make it work."</p>
 </div>
       </div>
       <div class="col-md-4">
+        <div class="mediumspace"></div>
+
         <div class='col-md-9 col-md-offset-2 text-center contact'>
-        <img src="img/pf/Bryan_225x300.png">
+        <img src="img/Bryan_225x300.png" height="250">
         <p>Bryan Nelissen</p>
         <p class="quote">"Why run, when u can lie in your bed."</p>
 </div>
       </div>
     </div>
-  </div>
+  <div class="main">
     <div class="row">
 
       <div class="col-md-4">
+        <div class="mediumspace"></div>
+
         <div class='col-md-9 col-md-offset-1 text-center contact'>
-        <img src="img/pf/Paulino_225x300.jpg">
+        <img src="img/Paulino_225x300.jpg" height="250">
         <p>J.J. dos Santos Paulino</p>
         <p class="quote">"One day or day one. You decide."</p>
 </div>
       </div>
       <div class="col-md-4">
+        <div class="mediumspace"></div>
+
         <div class='col-md-9 col-md-offset-2 text-center contact end'>
-        <img src="img/pf/michelle_225x300.png">
+        <img src="img/michelle_225x300.png" height="250">
         <p>Michelle Vrolijk</p>
         <p class="quote">"When life gives you lemons, make a cocktail."</p>
 </div>
       </div>
       <div class="col-md-4">
+        <div class="mediumspace"></div>
+
         <div class='col-md-9 col-md-offset-2 text-center contact end'>
-        <img src="img/pf/douwe_225x300.png">
+        <img src="img/douwe_225x300.png" height="250">
         <p>Douwe Schreuder</p>
         <p class="quote">"Assumption is the mother of all f#*! ups."</p>
 </div>
       </div>
     </div>
     </div>
+    </div>
+    </div>
   </div>
 </div>
-<?php include 'footer.php'; ?>
