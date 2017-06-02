17ae62cc96bbef220e4f5544d89b88a83298365d
RicardoHemprig <ricardo@360recruitment.nl>
2017-06-02 12:52:25 +0200

    Bullhorn page
    Added bullhorn page // not styled

---
+++ bullhorn.php
@@ -0,0 +1,48 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bullhorn.css">
+</head>
+
+
+<div class="col-md-12 page">
+  <div class='row'>
+
+    <div class='col-md-12 content col-lg-6 col-lg-offset-3'>
+      <img src="img/57ad39ddb8.png" class="center-block" style="height:150px; width:auto;">
+      <br>
+
+      De ervaren gebruikers en engineers van 360WEB kunnen u helpen bij het realiseren van een optimale Bullhorn inrichting.<br>
+Vanuit deze basis kan er gekeken worden wat uw specifieke wensen zijn voor de website en interne aangelegenheden.<br>
+Denk bijvoorbeeld aan een geautomatiseerde vacature pagina die realtime gekoppeld staat met Bullhorn.<br>
+Of dat u bepaalde performances van uw team in kaart wenst te brengen zodat de resultaten verbeteren. Maar ook specifieke wensen als pop-ups, werkverlichting d.m.v. automatisering en interactieve statistiek zijn bij ons mogelijk.
+De core van ons Bullhorn-team bestaat verschillende expertise. Vanuit directie, management en uitvoerend perspectief bekijken we de mogelijkheden. Zowel de software van Bullhorn als onze applicaties zijn zeer wendbaar en flexibel.
+Onze specialisten zijn tevens Bullhorn Canvas gecertificeerd.
+Hieronder treft u onze vanaf prijzen:
+<div class="smallspace"></div>
+
+      <div class='col-md-4'>
+        <div class="col-md-11 block">
+            <h3>€2.500</h3>
+            <p>Basis vacature integratie met de website</p>
+      </div>
+      </div>
+      <div class='col-md-4'>
+        <div class="col-md-11 col-md-offset-1 block">
+            <h3>€5.500</h3>
+            <p>Vacature integratie + nieuwe website</p>
+      </div>
+      </div>
+      <div class='col-md-4'>
+        <div class="col-md-11 col-md-offset-1 block">
+            <h3>Maatwerk €7.900</h3>
+            <p>Volledige eigen stijl inclusief een Bootstrap website</p>
+      </div>
+</div>
+</div>
+  </div>
+</div>
+
+
+
+<?php include('footer.php'); ?>
