7d83d0e6e9f10600e29d0dc40ec20920b6d140bc
RicardoHemprig <ricardo@360recruitment.nl>
2017-06-02 10:29:39 +0200

    New page
    Over ons pagina aangemaakt

---
+++ overons.php
@@ -0,0 +1,61 @@
+<?php require 'header.php'; ?>
+<head>
+<link href="css/overons.css" type="text/css" rel="stylesheet">
+</head>
+<div class="fluid-container testb0ss">
+  <div class="col-md-12">
+    <?php require 'navbar.php'; ?>
+    <div class="largespace"></div>
+    <div class="vertical-slide">
+    <div class="row">
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-1 text-center contact'>
+        <img src="img/pf/Robin_nieuw-225x300.png">
+        <p>Robin Veer</p>
+        <p class="quote">"You have to be perfect, to be amazing."</p>
+</div>
+      </div>
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-2 text-center contact'>
+        <img src="img/pf/Ricardo_255x300.jpg">
+        <p>Ricardo Hemprig</p>
+        <p class="quote">"Maybe swearing will make it work."</p>
+</div>
+      </div>
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-2 text-center contact'>
+        <img src="img/pf/Bryan_225x300.png">
+        <p>Bryan Nelissen</p>
+        <p class="quote">"Why run, when u can lie in your bed."</p>
+</div>
+      </div>
+    </div>
+  </div>
+    <div class="row">
+
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-1 text-center contact'>
+        <img src="img/pf/Paulino_225x300.jpg">
+        <p>J.J. dos Santos Paulino</p>
+        <p class="quote">"One day or day one. You decide."</p>
+</div>
+      </div>
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-2 text-center contact end'>
+        <img src="img/pf/michelle_225x300.png">
+        <p>Michelle Vrolijk</p>
+        <p class="quote">"When life gives you lemons, make a cocktail."</p>
+</div>
+      </div>
+      <div class="col-md-4">
+        <div class='col-md-9 col-md-offset-2 text-center contact end'>
+        <img src="img/pf/douwe_225x300.png">
+        <p>Douwe Schreuder</p>
+        <p class="quote">"Assumption is the mother of all f#*! ups."</p>
+</div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
