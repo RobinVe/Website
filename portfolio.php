9194af993ccfa4c40ef786ed27fc711e58a8a5cc
RobinVe <robin@360recruitment.nl>
2017-06-06 11:41:46 +0200

    Portfolio css

--- portfolio.php
+++ portfolio.php
@@ -1,53 +1,51 @@
 <?php require 'header.php'; ?>
-<link href="css/index.min.css" type="text/css" rel='stylesheet'>
-<link href="css/portfolio.css" type="text/css" rel='stylesheet'>
+<link href="css/portfolio.min.css" type="text/css" rel='stylesheet'>
 <style>
-
-
 </style>
 <div class="container-fluid">
-      <?php require 'navbar.php'; ?>
+   <?php require 'navbar.php'; ?>
 </div>
-
 <div class='double-slide'>
    <div class='main flex'>
-	   <h2 class='flex-center'>PORTFOLIO</h2>
+      <h2 class='flex-center'>PORTFOLIO</h2>
    </div>
    <div class='main'>
-       <div class='col-md-10 col-md-offset-1'>
-         <div class="lightbox-gallery">
-         				<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-						<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-						<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-						<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-						<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-						<div class="overlay">
-							<a href="https://www.360recruitment.nl">
-								<img class="portfolio" src="https://www.360recruitment.nl/360web/img/test.png">
-							</a>
-						</div>
-         		</div>
-       </div>
+      <div class='col-md-10 col-md-offset-1'>
+		  <div class='row'>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+	 </div>
+	 <div class='row'>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+         <div class="col-md-4 overlay">
+            <a href="https://www.360recruitment.nl">
+            <img class="portfolio center-block" src="https://www.360recruitment.nl/360web/img/test.png">
+            </a>
+         </div>
+         </div>
+      </div>
    </div>
 </div>
 <?php require 'footer.php'; ?>
