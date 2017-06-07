c960a8d6bf8d90c8b48fde7cbd7d445cef781a84
RobinVe <robin@360recruitment.nl>
2017-06-07 13:08:36 +0200

    Update

--- overons.php
+++ overons.php
@@ -3,55 +3,79 @@
    <link href="css/overons.min.css" type="text/css" rel="stylesheet">
 </head>
 <div class="container-fluid">
-	<div class='row'>
+   <div class='row'>
       <?php require 'navbar.php'; ?>
-         <div class="double-slide">
-            <div class="main">
-               <div class="col-md-4 flex">
-				   <div class='flex-center'>
+      <div class="double-slide">
+         <div class="main">
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
                      <img class='center-block' src="img/Robin_nieuw-225x300.png" height="250">
-                     <p class='text-center'>Robin Veer</p>
-                     <p class="quote">"You have to be perfect, to be amazing."</p>
-				 </div>
-               </div>
-               <div class="col-md-4 flex">
-				   <div class='flex-center'>
-                     <img class='center-block' src="img/Ricardo_255x300.jpg" height="250">
-                     <p class='text-center'>Ricardo Hemprig</p>
-                     <p class="quote">"Maybe swearing will make it work."</p>
-				 </div>
-               </div>
-               <div class="col-md-4 flex">
-				   <div class='flex-center'>
-                     <img class='center-block' src="img/Bryan_225x300.png" height="250">
-                     <p class='text-center'>Bryan Nelissen</p>
-                     <p class="quote">"Why run, when u can lie in your bed."</p>
-				 </div>
+                     <div class='text-block'>
+                        <p class='text-center'>Robin Veer</p>
+                        <p class="text-center">"You have to be perfect, to be amazing."</p>
+                     </div>
+                  </div>
                </div>
             </div>
-            <div class="main">
-                  <div class="col-md-4 flex">
-					  <div class='flex-center'>
-                        <img class='center-block' src="img/Paulino_225x300.jpg" height="250">
-                        <p class='text-center'>J.J. dos Santos Paulino</p>
-                        <p class="quote">"One day or day one. You decide."</p>
-					</div>
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
+                     <img class='center-block' src="img/Ricardo_255x300.jpg" height="250">
+                     <div class='text-block'>
+                        <p class='text-center'>Ricardo Hemprig</p>
+                        <p class="text-center">"Maybe swearing will make it work."</p>
+                     </div>
                   </div>
-                  <div class="col-md-4 flex">
-					  <div class='flex-center'>
-                        <img class='center-block' src="img/michelle_225x300.png" height="250">
-                        <p class='text-center'>Michelle Vrolijk</p>
-                        <p class="quote">"When life gives you lemons, make a cocktail."</p>
-					</div>
-                  </div>
-                  <div class="col-md-4 flex">
-					  <div class='flex-center'>
-                        <img class='center-block' src="img/douwe_225x300.png" height="250">
-                        <p class='text-center'>Douwe Schreuder</p>
-                        <p class="quote">"Assumption is the mother of all f#*! ups."</p>
-					</div>
                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
+                     <img class='center-block' src="img/Bryan_225x300.png" height="250">
+                     <div class='text-block'>
+                        <p class='text-center'>Bryan Nelissen</p>
+                        <p class="text-center">"Why run, when u can lie in your bed."</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
+         <div class="main">
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
+                     <img class='center-block' src="img/Paulino_225x300.jpg" height="250">
+                     <div class='text-block'>
+                        <p class='text-center'>J.J. dos Santos Paulino</p>
+                        <p class="text-center">"One day or day one. You decide."</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
+                     <img class='center-block' src="img/michelle_225x300.png" height="250">
+                     <div class='text-block'>
+                        <p class='text-center'>Michelle Vrolijk</p>
+                        <p class="text-center">"When life gives you lemons, make a cocktail."</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-4 col-xs-12 flex">
+               <div class='flex-center'>
+                  <div class='qoute-box'>
+                     <img class='center-block' src="img/douwe_225x300.png" height="250">
+                     <div class='text-block'>
+                        <p class='text-center'>Douwe Schreuder</p>
+                        <p class="text-center">"Assumption is the mother of all f#*! ups."</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
-         </div>
+      </div>
    </div>
+</div>
