3e0f7d79f86bfea96c0beb76110ab0621a274744
RobinVe <robin@360recruitment.nl>
2017-06-07 17:56:38 +0200

    updates plus package

--- starter.php
+++ starter.php
@@ -1,95 +1,61 @@
-<?php require 'header.php'; ?>
-<link href="css/index.min.css" type="text/css" rel='stylesheet'>
-<link href="css/pakketten.css" type="text/css" rel='stylesheet'>
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+<link href="css/pakketten.min.css" type="text/css" rel='stylesheet'>
+<head>
+    <title>Contact</title>
+</head>
+
 <style>
-
-
 </style>
-<div class="fluid-container">
-      <?php require 'navbar.php'; ?>
+<div class='container-fluid'>
+	<div class='double-slide row'>
+    <div class='main content-block'>
+            <div class='col-md-6 col-md-offset-3'>
+            <h1 class='text-center'>Starter Pakket</h1>
+            <h3 class='text-center underline'>moet hier nog een underline?</h3>
+        </div>
+    </div>
+
+    <div class='main'>
+        <div class='col-md-10 col-md-offset-1'>
+        <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1 content-block'>
+                <h3>VORMGEVING</h3>
+				<ul>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Bootstrap Platform</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Eigen design</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Formulieren</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Zoekfunctie</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Blog & Nieuwsberichten</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Testimonials</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Google Maps</span></li>
+					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Social Media deelbuttons</span></li>
+			</ul>
+            </div>
+        </div>
+        <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1 content-block'>
+                <h3>Proin sagittis</h3>
+                <p>Proin sagittis, urna eget euismod pretium, arcu leo finibus nisl, eu sagittis turpis arcu eu metus. Vivamus vel hendrerit odio. Vestibulum efficitur efficitur eros. Donec finibus gravida ligula vitae mollis. Quisque ut varius erat, iaculis convallis eros. Aliquam lobortis sodales eleifend. Quisque eros est, commodo sit amet tincidunt id, facilisis vitae tortor. Curabitur interdum dui quis felis porta ultricies. Phasellus eu metus id orci elementum vehicula. Sed scelerisque ligula a varius porta. Suspendisse placerat ornare felis, consequat hendrerit metus consectetur congue. Nam vehicula lectus ligula, sed porta dolor dapibus ultrices. Cras at tellus ut ipsum lobortis dictum at convallis mauris. Nunc egestas volutpat viverra.<p>
+            </div>
+        </div>
+        <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1 content-block'>
+                <h3>Curabitur quis</h3>
+                <p>Curabitur quis leo quis erat rhoncus sodales nec vitae ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, auctor id tellus et, lobortis dictum risus. Quisque commodo augue in velit cursus, bibendum consectetur libero imperdiet. Aliquam molestie vitae urna ac euismod. Cras accumsan ligula vel arcu varius, ut ornare lorem viverra. Etiam suscipit faucibus lorem sit amet varius. In vitae ullamcorper quam, vitae tempor dui. Integer quam augue, ultricies sed facilisis vel, tempus sit amet libero. Pellentesque molestie scelerisque augue eget tristique. Nunc eu urna vitae sapien tincidunt tempus a sed libero. Donec vitae dui porttitor, porta libero non, dictum augue.<p>
+            </div>
+        </div>
+        <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1 content-block'>
+                <h3>Cras accumsan</h3>
+                <p>Vitae tempor dui. Integer quam augue, ultricies sed facilisis vel, tempus sit amet libero. Pellentesque molestie scelerisque augue eget tristique. Nunc eu urna vitae sapien tincidunt tempus a sed libero. Donec vitae dui porttitor, porta libero non, dictum augue. Quis aliquet odio quam eget urna. Proin lacinia porttitor libero, placerat finibus tellus consequat nec. Pellentesque posuere, lectus vel auctor ornare, mi lacus pellentesque dui, vitae suscipit neque nunc nec risus. Cras vitae quam nulla. Morbi sit amet lacus non lorem ultricies auctor nec ut turpis. Proin iaculis pellentesque est, quis tempus purus iaculis a. Pellentesque imperdiet nisl a gravida laoreet.<p>
+            </div>
+        </div>
+    </div>
+    </div>
 </div>
 
-<div class='double-slide'>
-   <div class='main flex'>
-	   <h2 class='flex-center'>STARTER</h2>
-   </div>
-   <div class='main'>
-       <div class='col-md-10 col-md-offset-1'>
-         <div class='row'>
-    <div class='col-md-12 col-lg-3'>
-      <div class='col-md-10 col-md-offset-1 header'>
-        Vormgeving
-      </div>
-      <div class='content col-md-10 col-md-offset-1'>
-        <ul>
-          <li><i class="fa fa-check" aria-hidden="true"></i> WordPress CMS </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> Keuze uit Templates </li>
-        </ul>
-      </div>
-      <a href="https://www.360recruitment.nl/360web/basic">
-      <div class="col-md-10 col-md-offset-1 button">
-      bekijk basic
-    </div>
-  </a>
-    </div>
-    <div class='col-md-12 col-lg-3'>
-      <div class='col-md-10 col-md-offset-1 header'>
-        Data
-      </div>
-      <div class='content col-md-10 col-md-offset-1'>
-        <ul>
-          <li><i class="fa fa-check" aria-hidden="true"></i> 5 GB Dataverkeer </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> 8 GB Schrijfruimte </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> 1 MYSQL Database </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> Shared Server </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> Domeinregistratie </li>
-        </ul>
-      </div>
-      <div class='col-md-10 col-md-offset-1 button3'>
-      </div>
-    </div>
-    <div class='col-md-12 col-lg-3'>
-      <div class='col-md-10 col-md-offset-1 header'>
-        E-mail
-      </div>
-      <div class='content col-md-10 col-md-offset-1'>
-        <ul>
-          <li><i class="fa fa-check" aria-hidden="true"></i> 10 Emailboxen </li>
-          <li><i class="fa fa-check" aria-hidden="true"></i> 2 GB Email geheugen </li>
-        </ul>
-      </div>
-      <div class='col-md-10 col-md-offset-1 button3'>
-      </div>
-    </div>
-    <div class='col-md-12 col-lg-3'>
-      <div class='col-md-10 col-md-offset-1 header'>
-        Extra's
-      </div>
-      <div class='content col-md-10 col-md-offset-1'>
-        <ul>
-          <li><i class="fa fa-check" aria-hidden="true"></i> Full Service Support </li>
-        </ul>
-      </div>
-      <a href="https://www.360recruitment.nl/360web/premium">
-      <div class="col-md-10 col-md-offset-1 button">
-      bekijk premium
-    </div>
-  </a>
-    </div>
-    </div>
-    <div class="row">
-      <div class='col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3'>
-        <a href="https://www.360recruitment.nl/360web/aanvraag">
-          <div class="col-md-12 button2">
-            vraag offerte aan
-          </div>
-        </a>
-      </div>
-    </div>
-       </div>
-   </div>
-</div>
 
-<?php require 'footer.php'; ?>
-</div>
-</body>
+
+
+<?php include('footer.php'); ?>
