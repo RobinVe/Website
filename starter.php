40f5d6114293856893044ccc823e2d3bfd23c7f3
RobinVe <robin@360recruitment.nl>
2017-06-08 18:21:58 +0200

    8-6-17

--- starter.php
+++ starter.php
@@ -2,60 +2,93 @@
 <?php include('navbar.php'); ?>
 <link href="css/pakketten.min.css" type="text/css" rel='stylesheet'>
 <head>
-    <title>Contact</title>
+   <title>Contact</title>
 </head>
-
-<style>
-</style>
 <div class='container-fluid'>
-	<div class='double-slide row'>
-    <div class='main content-block'>
-            <div class='col-md-6 col-md-offset-3'>
-            <h1 class='text-center'>Starter Pakket</h1>
-            <h3 class='text-center underline'>moet hier nog een underline?</h3>
-        </div>
-    </div>
+<div class='double-slide row'>
+   <div class='main first-block'>
+      <div class='col-md-12'>
+	         <div class='col-md-4'>
+	            <h1 class='text-center'>Graag meer informatie ontvangen?</h1>
+	            <form>
+	               <div class="form-group col-sm-6">
+	                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Voornaam">
+	                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Achternaam">
+	               </div>
+	               <div class="form-group col-sm-6">
+	                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Email">
+	                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+	               </div>
+	               <div class="form-group col-sm-12">
+	                  <textarea class='form-control' placeholder="Bericht" rows="4"></textarea>
+	               </div>
+	               <div class="form-group col-sm-12">
+	                  <button type="submit" class="btn btn-default">
+	                     <p>Versturen</p>
+	                  </button>
+	               </div>
+	            </form>
+	         </div>
+	 		<div class='col-md-8'>
 
-    <div class='main'>
-        <div class='col-md-10 col-md-offset-1'>
-        <div class='col-md-3'>
-            <div class='col-md-10 col-md-offset-1 content-block'>
-                <h3>VORMGEVING</h3>
-				<ul>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Bootstrap Platform</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Eigen design</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Formulieren</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Zoekfunctie</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Blog & Nieuwsberichten</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Testimonials</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Google Maps</span></li>
-					<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Social Media deelbuttons</span></li>
-			</ul>
+				<div class='col-md-6 col-md-offset-6'>
+					<h1 class='text-center'>Starter Pakket</h1>
+
+					<p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit porttitor orci nec lacinia. Morbi nec malesuada lorem, vitae mattis mi. Mauris non tincidunt sapien. Aenean egestas commodo augue, scelerisque porta leo consequat vitae. Nullam ligula risus, vulputate vel scelerisque sit amet, eleifend et mi. Fusce ultricies massa id vestibulum scelerisque. Fusce sagittis eu quam eget mollis. Vestibulum scelerisque elit eu nibh efficitur volutpat. Quisque convallis nulla maximus magna consequat consequat. Nunc tincidunt diam id augue ultrices ornare. Etiam ultrices imperdiet dui a pulvinar.</p>
+
+					<p>Duis at augue a mi tempus dictum a et massa. Vestibulum quam orci, cursus sed tristique eget, consectetur eget lorem. Donec malesuada nunc nec venenatis gravida. Etiam tincidunt magna sagittis leo vulputate, ut vestibulum nisl tempor. Vestibulum nec nisi sodales, rutrum nisl non, tincidunt est. Integer quis consequat orci, id posuere turpis. Nullam ultrices aliquet consectetur.</i></p>
+				</div>
+
+			</div>
+      </div>
+   </div>
+   <div class='main'>
+      <div class='col-md-10 col-md-offset-1'>
+         <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1'>
+               <h3>Vormgeving</h3>
+               <ul>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Bootstrap Platform</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Eigen design</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Formulieren</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Zoekfunctie</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Blog & Nieuwsberichten</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Testimonials</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Google Maps</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Social Media deelbuttons</span></li>
+               </ul>
             </div>
-        </div>
-        <div class='col-md-3'>
-            <div class='col-md-10 col-md-offset-1 content-block'>
-                <h3>Proin sagittis</h3>
-                <p>Proin sagittis, urna eget euismod pretium, arcu leo finibus nisl, eu sagittis turpis arcu eu metus. Vivamus vel hendrerit odio. Vestibulum efficitur efficitur eros. Donec finibus gravida ligula vitae mollis. Quisque ut varius erat, iaculis convallis eros. Aliquam lobortis sodales eleifend. Quisque eros est, commodo sit amet tincidunt id, facilisis vitae tortor. Curabitur interdum dui quis felis porta ultricies. Phasellus eu metus id orci elementum vehicula. Sed scelerisque ligula a varius porta. Suspendisse placerat ornare felis, consequat hendrerit metus consectetur congue. Nam vehicula lectus ligula, sed porta dolor dapibus ultrices. Cras at tellus ut ipsum lobortis dictum at convallis mauris. Nunc egestas volutpat viverra.<p>
+         </div>
+         <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1'>
+               <h3>Data</h3>
+               <ul>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>5 GB Dataverkeer</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>8 GB Schrijfruimte</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>1 MYSQL Database</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Shared Server</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Domeinregistratie</span></li>
+               </ul>
             </div>
-        </div>
-        <div class='col-md-3'>
-            <div class='col-md-10 col-md-offset-1 content-block'>
-                <h3>Curabitur quis</h3>
-                <p>Curabitur quis leo quis erat rhoncus sodales nec vitae ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, auctor id tellus et, lobortis dictum risus. Quisque commodo augue in velit cursus, bibendum consectetur libero imperdiet. Aliquam molestie vitae urna ac euismod. Cras accumsan ligula vel arcu varius, ut ornare lorem viverra. Etiam suscipit faucibus lorem sit amet varius. In vitae ullamcorper quam, vitae tempor dui. Integer quam augue, ultricies sed facilisis vel, tempus sit amet libero. Pellentesque molestie scelerisque augue eget tristique. Nunc eu urna vitae sapien tincidunt tempus a sed libero. Donec vitae dui porttitor, porta libero non, dictum augue.<p>
+         </div>
+         <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1'>
+               <h3>Data</h3>
+               <ul>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>10 Emailboxen</span></li>
+                  <li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>2 GB Email geheugen</span></li>
+               </ul>
             </div>
-        </div>
-        <div class='col-md-3'>
-            <div class='col-md-10 col-md-offset-1 content-block'>
-                <h3>Cras accumsan</h3>
-                <p>Vitae tempor dui. Integer quam augue, ultricies sed facilisis vel, tempus sit amet libero. Pellentesque molestie scelerisque augue eget tristique. Nunc eu urna vitae sapien tincidunt tempus a sed libero. Donec vitae dui porttitor, porta libero non, dictum augue. Quis aliquet odio quam eget urna. Proin lacinia porttitor libero, placerat finibus tellus consequat nec. Pellentesque posuere, lectus vel auctor ornare, mi lacus pellentesque dui, vitae suscipit neque nunc nec risus. Cras vitae quam nulla. Morbi sit amet lacus non lorem ultricies auctor nec ut turpis. Proin iaculis pellentesque est, quis tempus purus iaculis a. Pellentesque imperdiet nisl a gravida laoreet.<p>
+         </div>
+         <div class='col-md-3'>
+            <div class='col-md-10 col-md-offset-1'>
+               <h3>Extra's</h3>
+			   	<ul>
+               		<li><i class="fa fa-check-circle-o" aria-hidden="true"></i><span>Full Service Support</span></li>
+		   		</ul>
             </div>
-        </div>
-    </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
-
-
-
-
 <?php include('footer.php'); ?>
