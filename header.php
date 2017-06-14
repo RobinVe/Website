7d1124a41ee0bf08da33fd14dd2bf07309e9a74f
RobinVe <robin@360recruitment.nl>
2017-06-14 15:52:58 +0200

    Favicons
    Updates the favicons

--- header.php
+++ header.php
@@ -6,8 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      
-    <link rel="shortcut icon" type="image/png" href="img/favicon/360web.png"/>
+
 
 	<?php
     if (isset($title)) {
@@ -17,6 +16,18 @@
     };
 
     ?>
+	<!-- Favicons -->
+
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="img/favicon/manifest.json">
+	<link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico">
+	<meta name="msapplication-config" content="img/favicon/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- Stylesheets and CDN  -->
 
     <link rel="stylesheet" href="<?php echo current_link();?>css/devicon.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
--- navbar.php
+++ navbar.php
@@ -9,17 +9,17 @@
            <span class="icon-bar"></span>
            <span class="icon-bar"></span>
          </button>
-		 <a class="navbar-brand" href="<?php echo current_link();?>index.php"><img class="center-block" src="<?php echo current_link();?>img/360web.png" height="175"></a>
+		 <a class="navbar-brand" href="<?php echo current_link();?>index"><img class="center-block" src="<?php echo current_link();?>img/360web.png" height="175"></a>
        </div>
        <div class="collapse navbar-collapse navbar" id="navbar-collapse-1">
          <ul class="nav navbar-nav">
-           <li><a href="<?php echo current_link();?>index.php">Home</a></li>
-           <li><a href="<?php echo current_link();?>portfolio.php">Portfolio</a></li>
-           <li><a href="<?php echo current_link();?>index.php#Pakketten">Pakketten</a></li>
-           <li><a href="<?php echo current_link();?>index.php#Full-Service">Full Service</a></li>
-           <li><a href="<?php echo current_link();?>overons.php">Over ons</a></li>
-           <li><a href="<?php echo current_link();?>index.php#Contact">Contact</a></li>
-           <li class="bullhornknop"><a href="bullhorn.php"><img style='float:left;' src="<?php echo current_link();?>img/BH-bull-orange.png" height="25"><span class='visible-xs' style='float:left;'>Bullhorn</span></a></li>
+           <li><a href="<?php echo current_link();?>">Home</a></li>
+           <li><a href="<?php echo current_link();?>portfolio">Portfolio</a></li>
+           <li><a href="<?php echo current_link();?>index#Pakketten">Pakketten</a></li>
+           <li><a href="<?php echo current_link();?>index#Full-Service">Full Service</a></li>
+           <li><a href="<?php echo current_link();?>overons">Over ons</a></li>
+           <li><a href="<?php echo current_link();?>index#Contact">Contact</a></li>
+           <li class="bullhornknop"><a href="bullhorn"><img style='float:left;' src="<?php echo current_link();?>img/BH-bull-orange.png" height="25"><span class='visible-xs' style='float:left;'>Bullhorn</span></a></li>
          </ul><!--/.nav-collapse -->
        </div><!--/.nav-collapse -->
      </div>
